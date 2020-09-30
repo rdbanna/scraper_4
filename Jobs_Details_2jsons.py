2d1ad440400dfa2a012eb406d423c096c3bcd5a9
rdbanna <72146409+rdbanna@users.noreply.github.com>
2020-10-01 00:02:47 +0530

    Update Jobs_Details_2jsons.py

--- Jobs_Details_2jsons.py
+++ Jobs_Details_2jsons.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-
+//this is my edit
 
 def data2_collector(inner_url):
     inner_soup = BeautifulSoup(requests.get(inner_url).content, "html.parser")
